9649189d89afcf2c531c5d2c80e4beae2e40d824
Harvey Decapia <harveydecapia@gmail.com>
2022-02-28 22:30:32 +0800

    Add sample spec
    should add new contact

--- test/pages/contactScreen.ts
+++ test/pages/contactScreen.ts
@@ -9,12 +9,20 @@ class ContactScreen {
         return $('id=text')
     }
 
-    get cancelAddAccountPopup(){
+    get addAccountCancenBtn(){
         return $('id=left_button')
+    }
+
+    get contactsLbl(){
+        return $('android=new UiSelector().text("Contacts")')
     }
 
     get firstNameTxtbox(){
         return $('android=new UiSelector().text("First name")')
+    }
+
+    get lastNameTxtbox(){
+        return $('android=new UiSelector().text("Last name")')
     }
 }
 
--- test/specs/app/app.contacts.spec.ts
+++ test/specs/app/app.contacts.spec.ts
@@ -2,16 +2,37 @@ import ContactScreen from "../../pages/contactScreen";
 
 
 describe('Android Native Contact App', () => {
-    
-    it('should add new contact', async () => {
+
+    beforeEach(() => {
+        //TODO: Close the app if open, then open the app - FULL RESET
+    });
+
+
+    it('should load the contacts native app', async () => {
+        
+        //Assert
+        expect(await ContactScreen.contactsLbl).toBeDisplayedInViewport()
+        expect(await ContactScreen.addBtn).toBeDisplayedInViewport()
+    });
+
+    it.only('should add new contact', async () => {
         //Act
         await (await ContactScreen.addBtn).click()
-        
         if(await ContactScreen.addAccountPopup.isDisplayed()){
-            await ContactScreen.cancelAddAccountPopup.click()
+            await ContactScreen.addAccountCancenBtn.click()
         }
 
         //Assert
         expect(await ContactScreen.firstNameTxtbox).toBeDisplayedInViewport()
+
+        //TEST
+        await (await ContactScreen.firstNameTxtbox).setValue('Satoshi')
+        await (await ContactScreen.lastNameTxtbox).setValue('Nakamoto')
+
+        await browser.hideKeyboard()
+
+        expect(await ContactScreen.firstNameTxtbox).toHaveValue('Satoshi')
+        expect(await ContactScreen.lastNameTxtbox).toHaveValue('Nakamoto')
+
     });
 });
